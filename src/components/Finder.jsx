5770e5494fb96b63a35675e8384543db35eee0f1
Aurora3001 <bero_22@alu.uabcs.mx>
2024-09-26 23:55:13 -0700

    componentes
    creacion de componentes y modificacion en app.jsx

--- src/components/Finder.jsx
+++ src/components/Finder.jsx
@@ -1,22 +1,21 @@
 function Finder({
-    finderLabel = "Buscador",
-    buttonText = "Buscar",
-    setBusqueda,
-    setIniciarBusqueda
-  }) {
-  
-  
-    const onClickButton = () =>{
-      setIniciarBusqueda(true)
-    }
-    return (
-      <>
-        <label htmlFor="">{finderLabel}</label>
-        <input type="text" onChange={(e) => setBusqueda(e.target.value)} />
-        <input type="button" value={buttonText} onClick={(e)=>setIniciarBusqueda(true)} />
-      </>
-    );
+  finderLabel = "Buscador",
+  buttonText = "Buscar",
+  setBusqueda,
+  setIniciarBusqueda
+}) {
+
+
+  const onClickButton = () =>{
+    setIniciarBusqueda(true)
   }
-  
-  export default Finder;
-  
+  return (
+    <>
+      <label htmlFor="">{finderLabel}</label>
+      <input type="text" onChange={(e) => setBusqueda(e.target.value)} />
+      <input type="button" value={buttonText} onClick={(e)=>setIniciarBusqueda(true)} />
+    </>
+  );
+}
+
+export default Finder;
